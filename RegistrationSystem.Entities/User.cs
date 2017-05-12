4fd43bbef050eb66bb166562d5045d6a7e9f0512
UserSer <UserSer@DESKTOP-9Q8NKBR>
2017-05-12 15:10:45 +0300

    Add User

---
+++ RegistrationSystem.Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Index(IsUnique = true)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Password { get; set; }
+
+        //navigation
+        public ICollection<Order> Orders { get; set; }
+    }
+}
