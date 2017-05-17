363a92f143192137b5b456afc6190d31fa00ea8a
UserSer <UserSer@DESKTOP-9Q8NKBR>
2017-05-17 10:29:50 +0300

    Revert "Global(reload)"
    This reverts commit 94a10a24c9a85b9c95ef362f4377076ec25e13b7.

---
+++ RegistrationSystem.Entities1/HelpEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.Entities
+{
+    public enum Sex
+    {
+        Чоловіча,
+        Жіноча
+    }
+
+    public enum District
+    {
+        Личаківський,
+        Сихівський,
+        Шевченківський,
+        Франківський,
+        Залізничний,
+        Галицький
+    }
+}
